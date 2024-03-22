4fdf818d73ea50284cfb2a1a30a22b41e6767b7b
Hogan McDonald <hogan.developer@gmail.com>
2024-03-22 15:15:55 -0500

    Adds Files util module
    it provides methods for:
    
    1. checking if a file exists.
    2. getting the path for the current file.
    3. getting the project root path - this takes into account the location
       of the Gemfile.
    4. opening a file in the current window.

---
+++ lua/rails-rspec-toggle/util/files.lua
@@ -0,0 +1,38 @@
+local Files = {}
+
+---Returns true if file exists
+---
+---@param path string
+---@return boolean
+Files.exists = function(path)
+  return vim.fn.filereadable(path) == 1
+end
+
+---Returns the current file path
+---
+---@return string
+Files.current_path = function()
+  return vim.api.nvim_buf_get_name(0)
+end
+
+---Returns the path of the Rails root directory (location of the Gemfile)
+---
+---@return string
+Files.root_path = function()
+  local path = vim.fn.findfile('Gemfile', vim.fn.fnamemodify(Files.current_path(), ":p") .. ";")
+  if path and #path > 0 then
+    path = vim.fn.fnamemodify(path, ":p:h")
+  end
+  return path
+end
+
+---Opens file at path if it exists
+---
+---@param path string
+Files.open = function(path)
+  if Files.exists(path) then
+    vim.cmd('e ' .. path)
+  end
+end
+
+return Files
