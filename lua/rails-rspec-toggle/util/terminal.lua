097892924450a051f29717c2af4ee471b5c58441
Hogan McDonald <hogan.developer@gmail.com>
2024-03-22 15:58:55 -0500

    creates a module for running commands in a terminal split
    Terminal.run will recieve a command and a callback method. It will then
    open a split with height of 24 and run the terminal command in it. After
    the command has finished it will close the split and call the callback.

---
+++ lua/rails-rspec-toggle/util/terminal.lua
@@ -0,0 +1,27 @@
+local Files = require('rails-rspec-toggle.util.files')
+
+local Terminal = {}
+
+local term = nil
+
+---Runs command in a split terminal. When command completes, terminal split
+---closes and callback is executed.
+---
+---@param command string
+---@param callback function
+Terminal.run = function(command, callback)
+  local opts = {
+    cwd = Files.root_path(),
+    on_exit = function()
+      if term and vim.fn.bufexists(term) > 0 then
+        vim.api.nvim_buf_delete(term, { force = true })
+      end
+      callback()
+    end
+  }
+  vim.cmd('botright 24split new')
+  term = vim.api.nvim_get_current_buf()
+  vim.fn.termopen(command, opts)
+end
+
+return Terminal
