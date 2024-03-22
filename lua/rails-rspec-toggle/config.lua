d737bae507c554fc7dd5d2e34abb1bc0d0b2f0a3
Hogan McDonald <hogan.developer@gmail.com>
2024-03-22 16:02:11 -0500

    adds defaults to config that includes a map of generator commands based on file type

--- lua/rails-rspec-toggle/config.lua
+++ lua/rails-rspec-toggle/config.lua
@@ -3,11 +3,25 @@ local M = {}
 ---@class Options
 ---@field spec_directory string
 ---@field spec_file_suffix string
+---@field generator_map table
 
 ---@type Options
 local defaults = {
-  spec_directory = "test",
-  spec_file_suffix = "_test.rb"
+  spec_directory = 'spec',
+  spec_file_suffix = '_spec.rb',
+  generator_map = {
+    channel = 'rspec:channel',
+    controller = 'rspec:controller',
+    feature = 'rspec:feature',
+    helper = 'rspec:helper',
+    integration = 'rspec:integration',
+    job = 'rspec:job',
+    mailbox = 'rspec:mailbox',
+    mailer = 'rspec:mailer',
+    model = 'rspec:model',
+    request = 'rspec:request',
+    view = 'rspec:view',
+  },
 }
 
 ---@type Options
