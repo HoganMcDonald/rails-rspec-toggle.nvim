cfd13b10e23d68f41f094a486b35589622450241
Hogan McDonald <hogan.developer@gmail.com>
2024-03-22 14:58:58 -0500

    creates a util module for dealing with Rails specific utilities
    This includes:
    1. A method for determining if a given file path in a rails
       repo is a spec file or an app file.
    2. A method that takes a partial path and returns the spec file full
       path.
    3. A method that takes a partial path and returns the appropriate
       generator command to create the spec file.

---
+++ lua/rails-rspec-toggle/util/rails.lua
@@ -0,0 +1,37 @@
+local options = require('rails-rspec-toggle.config').options
+local Files = require('rails-rspec-toggle.util.files')
+
+local Rails = {}
+
+---Returns "spec" or "app" based on the directory file_path is located in
+---
+---@param file_path string
+---@return string|nil "spec" or "app"
+Rails.file_type = function(file_path)
+  if string.match(file_path, "^" .. Files.root_path() .. "/spec/") then
+    return 'spec'
+  elseif string.match(file_path, "^" .. Files.root_path() .. "/app/") then
+    return 'app'
+  end
+end
+
+---takes a partial path and returns the spec file path.
+---
+---@param partial_path string
+---@return string
+Rails.spec_file_path = function(partial_path)
+  return '/' .. options.spec_directory .. '/' .. partial_path .. options.spec_file_suffix
+end
+
+---takes a partial path and calls the correct generator based on spec directory.
+---runs command in a terminal split.
+---
+---@param partial_path string
+---@return string
+Rails.generate = function(partial_path)
+  local spec_type = string.match(partial_path, "^(.*)s%/")
+  local app_file = string.match(partial_path, "/(.*)$")
+  return 'bundle exec rails g ' .. options.generator_map[spec_type] .. ' ' .. app_file
+end
+
+return Rails
